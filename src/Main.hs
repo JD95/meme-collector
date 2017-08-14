94b8e0ab9d6ec5cb2eaf73e04278a125670de846
Jeff <jeffreydwyer95@hotmail.com>
2017-08-13 22:42:35 -0700

    Parse picture urls and generates possible completions for indirect links

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import Prelude ()
 import Protolude
 import Control.Arrow
 import Data.Maybe
+import qualified Data.Attoparsec.Text as A
 import qualified Reddit as R
 import qualified Reddit.Types as RT
 import qualified Reddit.Types.Listing as RT
@@ -52,12 +53,46 @@ collect :: RT.Reddit ()
 collect = do
   info <- RA.getPosts' (RT.Options Nothing Nothing) RT.New (Just $ RT.R "AdviceAnimals")
   liftIO . (mapM_ . mapM) print
-         . traverse extractLinks
+         . fmap extractLinks
          . RT.contents $ info
 
-extractLinks :: RT.Post -> Maybe (Text, Text)
+extractLinks :: RT.Post -> Maybe (Text, [Text])
 extractLinks post =
   case RT.content post of
-    (RT.Link l) -> Just (RT.title post, l)
+    (RT.Link l) ->  sequence (RT.title post, parseImgSource (l <> "\n"))
     _ -> Nothing
+
+
+toImageSite :: Text -> Maybe Text
+toImageSite "imgur.com" = Just "i.imgur.com"
+toImageSite "imgflip.com" = Just "i.imgflip.com"
+toImageSite _ = Nothing
+
+endings = [ ".jpg"
+          , ".png"
+          ]
+          
+attatchEndings t = fmap (t <>) endings
+
+parseExtention :: Text -> Maybe [Text]
+parseExtention url =
+  maybe Nothing (const $ Just [url])
+  . A.maybeResult
+  . A.parse (A.manyTill A.anyChar (A.choice (fmap A.string endings)))
+  $ url
+
+
+parseNoExtention = fmap attatchEndings . join . A.maybeResult . A.parse (do
+  http <- A.string "https" <|> A.string "http"
+  colonSlash <- "://"
+  site <- A.takeTill ('/' ==)
+  slash <- A.string "/"
+  A.option "" (A.string "i/")
+  imgHash <- A.manyTill' A.anyChar A.endOfLine
+  pure $ (\s -> http <> colonSlash <> s <> slash <> toS imgHash) <$> toImageSite site)
+
+
+parseImgSource url = parseExtention url <|> parseNoExtention url
+
 
+  
